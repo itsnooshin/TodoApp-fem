f479fa7771ec4d182cabf8e552b2d49049aba344
Nooshin bakhtiari <nooshindev@gmail.com>
2023-07-10 14:24:11 +0330

    Add functionality for users to add todo items and track the count of created items

--- js/main.js
+++ js/main.js
@@ -1,41 +1,42 @@
 // create elements
 "use strict";
 const newList = document.querySelector(".main__form__input");
-const containerList = document.querySelector(".main__box-otherlist");
+const containerList = document.querySelectorAll(".main__box-otherlist");
 const checkList = document.getElementsByClassName("main__box--check");
 const checkListParagh = document.getElementsByClassName("box__lists");
 const buttonSpan = document.getElementById("mySpan");
+const itemLeft = document.querySelector(".item-calcuted");
 
 // when user add a to do display it on list order
+
 newList.addEventListener("keydown", function (e) {
   if (e.key === "Enter") {
-    console.log(e.target.typeof === "input");
     const html = `
+    <div class="main__box-otherlist">
     <div class="main__box-item">
       <span class="main__box--check"></span>
-      <p class="box__lists">${e.target.value}</p>
-  </div>`;
+      <p class="box__lists"></p>
+    </div>
+  </div>
+    `;
+    const newElement = document.createElement("div");
+    newElement.innerHTML = html;
+    containerList.forEach((element) => {
+      element.appendChild(newElement);
+      const newItem = element.lastChild;
+      const p = newItem.querySelector(".box__lists");
+      p.innerHTML = e.target.value;
 
-    containerList.insertAdjacentHTML("afterbegin", html);
+      itemLeft.textContent = element.children.length;
 
-    e.target.value = "";
-    let select = null;
-    for (let i = 0; i < checkList.length; i++) {
-      checkList[i].addEventListener("click", function (e) {
-       
-        console.log(e.target);
-
-         if(!checkListParagh[i].classList.contains('text-through')){
-          checkListParagh[i].classList.add("text-through");
-         }
-        // else{
-          
-        // }
-        
-        // select = checkListParagh[i];
+      p.style.color = "#c8cbe7";
+      const span = newItem.querySelector(".main__box--check");
+      p.innerHTML = e.target.value;
+      span.addEventListener("click", function () {
+        p.classList.toggle("text-through");
       });
-    }
+    });
 
-  
+    e.target.value = ""; // for remove the item in new list
   }
 });
